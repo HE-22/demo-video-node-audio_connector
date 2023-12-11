2ab179a8feafe5f1046dd8c26d06f467fd7c964e
HE-22 <134304564+HE-22@users.noreply.github.com>
2023-12-11 10:38:59 -0500

    call audio streams sent to python websocket server

--- controllers/symbl-controller.js
+++ controllers/symbl-controller.js
@@ -1,134 +1,184 @@
 exports.postSymblCall = (ctx) => {
-	let otSession = ctx.openTokSession;
-	let token = otSession.generateToken();
-	let meetingLinkURI = `http://${process.env.TUNNEL_DOMAIN}/join-call`;
-	let symblProcessor = ctx.symblProcessor;
-	let configOptions = ctx.request.body.config;
-	let config = [];
-	if(typeof configOptions == "string") { config.push(configOptions) }
-	if(Array.isArray(configOptions)) { config.push(...configOptions) }
-	symblProcessor.setConfig(config);
+  let otSession = ctx.openTokSession;
+  let token = otSession.generateToken();
+  let meetingLinkURI = `http://${process.env.TUNNEL_DOMAIN}/join-call`;
+  let symblProcessor = ctx.symblProcessor;
+  let configOptions = ctx.request.body.config;
+  let config = [];
+  if (typeof configOptions == "string") {
+    config.push(configOptions);
+  }
+  if (Array.isArray(configOptions)) {
+    config.push(...configOptions);
+  }
+  symblProcessor.setConfig(config);
 
-  return ctx.render('symbl-call', {
-		apiKey: otSession.ot.apiKey,
+  return ctx.render("symbl-call", {
+    apiKey: otSession.ot.apiKey,
     sessionId: otSession.sessionId,
     token: token,
-		meetingLink: meetingLinkURI,
-		config: config
-	});
+    meetingLink: meetingLinkURI,
+    config: config,
+  });
 };
 
 exports.postSymblProcessing = async (ctx, next) => {
-	let opentok = ctx.opentok;
-	let otSession = ctx.openTokSession;
-	let token = otSession.generateToken();
-	let socketURI = `wss://${process.env.TUNNEL_DOMAIN}/socket`;
-	let symblProcessor = ctx.symblProcessor;
-	let symblSdk = ctx.symblSdk;
-	let ws = ctx.ws;
-	let insightTypes = symblProcessor.setInsightTypes();
-	let handlers = symblProcessor.sethandlers();
+  let opentok = ctx.opentok;
+  let otSession = ctx.openTokSession;
+  let token = otSession.generateToken();
+  let socketURI = `wss://${process.env.TUNNEL_DOMAIN}/socket`;
+  let symblProcessor = ctx.symblProcessor;
+  let symblSdk = ctx.symblSdk;
+  let ws = ctx.ws;
+  let insightTypes = symblProcessor.setInsightTypes();
+  let handlers = symblProcessor.sethandlers();
 
-	opentok.listStreams(otSession.sessionId, function(error, streams) {
-		if (error) {
-			console.log('Error:', error.message);
-		} else {
-			streams.forEach(async stream => {
-				let stream_id = stream.id;
-				let stream_name = stream.name;
-				let symblConnection;
-				let socketUriForStream = socketURI + '/' + stream_id;
+  opentok.listStreams(otSession.sessionId, function (error, streams) {
+    if (error) {
+      console.log("Error:", error.message);
+    } else {
+      streams.forEach(async (stream) => {
+        let stream_id = stream.id;
+        let stream_name = stream.name;
+        let symblConnection;
+        let socketUriForStream = socketURI + "/" + stream_id;
 
-				console.log("connecting to symbl for: ", stream_id);
-				try {
-					symblConnection = await symblSdk.startRealtimeRequest({
-						id: stream_id,
-						speaker: {
-							name: stream_name
-						},
-						insightTypes: insightTypes,
-						config: {
-							meetingTitle: 'My Test Meeting',
-							confidenceThreshold: 0.9,
-							timezoneOffset: 0, // Offset in minutes from UTC
-							languageCode: 'en-GB',
-							sampleRateHertz: 16000,
-						},
-						handlers: handlers
-					});
-					console.log("Connected to Symbl for: ", symblConnection);
+        console.log("connecting to symbl for: ", stream_id);
+        try {
+          symblConnection = await symblSdk.startRealtimeRequest({
+            id: stream_id,
+            speaker: {
+              name: stream_name,
+            },
+            insightTypes: insightTypes,
+            config: {
+              meetingTitle: "My Test Meeting",
+              confidenceThreshold: 0.9,
+              timezoneOffset: 0, // Offset in minutes from UTC
+              languageCode: "en-GB",
+              sampleRateHertz: 16000,
+            },
+            handlers: handlers,
+          });
+          console.log("Connected to Symbl for: ", symblConnection);
+        } catch (e) {
+          console.log("Symbl connect error");
+          console.error(e);
+        }
 
-				} catch (e) {
-					console.log("Symbl connect error");
-					console.error(e);
-				}
+        console.log("before ws.get");
 
-				ws.get(`/socket/${stream_id}`, ctx => {
-					let connection = symblConnection;
-					ctx.websocket.on('message', function(message) {
+        // send audio to python socket server instead of symbl
+        // Make sure you have the WebSocket library imported
+        const WebSocket = require("ws");
 
-						try {
-							const event = JSON.parse(message);
-							if (event.event === 'websocket:connected') {
-								console.log(event);
-							}
-						} catch(err) {
-							if (connection) {
-								connection.sendAudio(message);
-								return;
-							}
-						}
+        ws.get(`/socket/${stream_id}`, (ctx) => {
+          let connection = symblConnection;
 
-					});
+          // Create a WebSocket connection to the Python server
+          let pythonServerUri = `ws://localhost:8765/socket/${stream_id}`;
+          let pythonServerSocket;
 
-				});
+          try {
+            pythonServerSocket = new WebSocket(pythonServerUri);
 
-				opentok.websocketConnect(otSession.sessionId, token, socketUriForStream, {streams: [stream_id]}, function(error, socket) {
-					if (error) {
-						console.log('Error:', error.message);
-					} else {
-						console.log('OpenTok Socket websocket connected');
-					}
-				});
+            // Handle the open event
+            pythonServerSocket.on("open", () => {
+              console.log("Connection to Python server established");
+            });
 
-			});
-		}
-	});
+            // Handle potential errors
+            pythonServerSocket.on("error", (error) => {
+              console.error("Error in WebSocket connection:", error);
+            });
+          } catch (e) {
+            console.error(
+              "Failed to create WebSocket connection to Python server:",
+              e
+            );
+          }
 
-	ctx.status = 200;
+          ctx.websocket.on("message", function (message) {
+            try {
+              const event = JSON.parse(message);
+              if (event.event === "websocket:connected") {
+                console.log(event);
+              }
+            } catch (err) {
+              console.log("about to check if pythonServerSocket is open");
+              try {
+                if (
+                  pythonServerSocket &&
+                  pythonServerSocket.readyState === WebSocket.OPEN
+                ) {
+                  pythonServerSocket.send(message);
+                } else {
+                  console.log(
+                    "pythonServerSocket is not open or not available"
+                  );
+                }
+              } catch (e) {
+                console.error("Failed to send message to Python server:", e);
+              }
+            }
+          });
+
+          // Handle the close event
+          ctx.websocket.on("close", () => {
+            if (pythonServerSocket) {
+              pythonServerSocket.close();
+            }
+            console.log("WebSocket connection closed");
+          });
+        });
+
+        opentok.websocketConnect(
+          otSession.sessionId,
+          token,
+          socketUriForStream,
+          { streams: [stream_id] },
+          function (error, socket) {
+            if (error) {
+              console.log("Error:", error.message);
+            } else {
+              console.log("OpenTok Socket websocket connected");
+            }
+          }
+        );
+      });
+    }
+  });
+
+  ctx.status = 200;
 };
 
 exports.getSymblTranscription = (ctx) => {
-	let symblProcessor = ctx.symblProcessor;
-	return ctx.render('symbl-transcriptions', {
-		transcriptions: symblProcessor.getTranscriptions()
-	}
- );
+  let symblProcessor = ctx.symblProcessor;
+  return ctx.render("symbl-transcriptions", {
+    transcriptions: symblProcessor.getTranscriptions(),
+  });
 };
 
 exports.getSymblActionItems = (ctx) => {
-	let symblProcessor = ctx.symblProcessor;
-	return ctx.render('symbl-output', {
-		heading: 'Action Items',
-		list: symblProcessor.getActionItems()
-	}
- );
+  let symblProcessor = ctx.symblProcessor;
+  return ctx.render("symbl-output", {
+    heading: "Action Items",
+    list: symblProcessor.getActionItems(),
+  });
 };
 
 exports.getSymblQuestions = (ctx) => {
-	let symblProcessor = ctx.symblProcessor;
-	return ctx.render('symbl-output', {
-		heading: 'Questions',
-		list: symblProcessor.getQuestions()
-	}
- );
+  let symblProcessor = ctx.symblProcessor;
+  return ctx.render("symbl-output", {
+    heading: "Questions",
+    list: symblProcessor.getQuestions(),
+  });
 };
 
 exports.getSymblTopics = (ctx) => {
-	let symblProcessor = ctx.symblProcessor;
-	return ctx.render('symbl-output', {
-		heading: 'Topics',
-		list: symblProcessor.getTopics()
-	}
- );
+  let symblProcessor = ctx.symblProcessor;
+  return ctx.render("symbl-output", {
+    heading: "Topics",
+    list: symblProcessor.getTopics(),
+  });
 };
