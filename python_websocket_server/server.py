1dcf024300e45ea37e4e34eb73a9ca577ece58d1
HE-22 <134304564+HE-22@users.noreply.github.com>
2023-12-17 17:50:46 -0500

    attempt to setup HLS server take in audio and setup HLS server, not working though so chanign to a flask server

--- python_websocket_server/server.py
+++ python_websocket_server/server.py
@@ -1,93 +1,49 @@
+# server.py
+
 import asyncio
 import websockets
 import logging
-import numpy as np
-from io import BytesIO
-import whisper
-import os
-from collections import deque
-from datetime import datetime, timedelta
-from colorama import Fore, Style
-
-# Load the Whisper model for real-time transcription
-model = whisper.load_model("tiny")
+from hls_stream_manager import HLSStreamManager
 
 # Server configuration
 SERVER_HOST = "localhost"
 SERVER_PORT = 8765
+HLS_OUTPUT_DIR = "/Users/hassen/Dev/Jeenie/demo-video-node-audio_connector/python_websocket_server/hls_content"  # Set the directory where HLS files will be stored
+HTTP_SERVER_PORT = 8000  # Port for the HTTP server to serve HLS content
 
-# Audio stream configuration
-SAMPLE_WIDTH = 2  # Assuming 16-bit audio
-FRAME_RATE = 16000  # Sample rate
-CHANNELS = 1  # Mono audio
+# Dictionary to track HLSStreamManager instances for each stream_id
+hls_managers = {}
 
-# Buffer and transcription configuration
-BUFFER_DURATION = 10  # Buffer duration in seconds
-SILENCE_THRESHOLD = 500  # Silence threshold for audio chunk
-PHRASE_TIMEOUT = 3  # Timeout to consider end of phrase
-
-# Initialize a queue for audio data
-audio_queue = deque()
-last_audio_time = None
-
-# Set of connected websockets
-connected = set()
-
-logging.basicConfig(
-    format="%(asctime)s %(levelname)s:%(message)s",
-    level=logging.INFO,
-    datefmt="%H:%M:%S",
-)
+logging.basicConfig(level=logging.INFO)
 
 
 async def handle_audio(websocket, path):
-    global last_audio_time
-    connected.add(websocket)
-    buffer = BytesIO()
+    stream_id = path.replace(
+        "/socket/", "", 1
+    )  # TODO: Stream Id still broken, shows up like 'socket/c01123d6-1328-4e65-a641-6f4ec156b5c1' (check again)
+    logging.info(f"Stream {stream_id} connected")
+
+    # Create a new HLSStreamManager for the stream_id if it doesn't exist
+    if stream_id not in hls_managers:
+        hls_managers[stream_id] = HLSStreamManager(
+            stream_id, HLS_OUTPUT_DIR, SERVER_HOST, HTTP_SERVER_PORT
+        )
+        hls_url = hls_managers[stream_id].get_hls_playlist_url()
+        logging.info(f"HLS URL for stream {stream_id}: {hls_url}")
 
     try:
         async for message in websocket:
             if isinstance(message, bytes):
-                buffer.write(message)
-                last_audio_time = datetime.now()
-
-            # Transcribe if buffer is full or if silence is detected
-            current_time = datetime.now()
-            if (
-                buffer.getbuffer().nbytes
-                >= BUFFER_DURATION * FRAME_RATE * SAMPLE_WIDTH * CHANNELS
-            ) or (
-                last_audio_time
-                and (current_time - last_audio_time) > timedelta(seconds=PHRASE_TIMEOUT)
-            ):
-                # Reset last audio time
-                last_audio_time = None
-
-                # Process and transcribe buffered audio
-                buffer.seek(0)
-                audio_np = (
-                    np.frombuffer(buffer.read(), dtype=np.int16).astype(np.float32)
-                    / 32768.0
-                )
-                result = model.transcribe(audio_np)
-                transcription = result["text"].strip()
-
-                logging.info(
-                    f"Transcribed Text: {Fore.GREEN}{transcription}{Style.RESET_ALL}"
-                )
-
-                # Reset buffer for next audio stream
-                buffer = BytesIO()
-
+                hls_managers[stream_id].write_audio_data(message)
     except websockets.exceptions.ConnectionClosed:
-        logging.info("Connection closed")
-    finally:
-        connected.remove(websocket)
+        logging.info(f"Stream {stream_id} connection closed")
+        hls_managers[stream_id].cleanup()
+        del hls_managers[stream_id]
 
 
 async def start_server():
     async with websockets.serve(handle_audio, SERVER_HOST, SERVER_PORT):
-        logging.info(f"Server started on ws://{SERVER_HOST}:{SERVER_PORT}")
+        logging.info(f"WebSocket server started on ws://{SERVER_HOST}:{SERVER_PORT}")
         await asyncio.Future()  # Run server indefinitely
 
 
