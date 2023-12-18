8e9b7f23607173c30c927cee05b156b66d9b33ed
HE-22 <134304564+HE-22@users.noreply.github.com>
2023-12-17 19:04:16 -0500

    still broken
    server sending off to HLS server..

--- python_websocket_server/server.py
+++ python_websocket_server/server.py
@@ -3,45 +3,46 @@
 import asyncio
 import websockets
 import logging
-from hls_stream_manager import HLSStreamManager
 
 # Server configuration
 SERVER_HOST = "localhost"
 SERVER_PORT = 8765
-HLS_OUTPUT_DIR = "/Users/hassen/Dev/Jeenie/demo-video-node-audio_connector/python_websocket_server/hls_content"  # Set the directory where HLS files will be stored
-HTTP_SERVER_PORT = 8000  # Port for the HTTP server to serve HLS content
-
-# Dictionary to track HLSStreamManager instances for each stream_id
-hls_managers = {}
+FLASK_SERVER_WS_URI = "ws://localhost:5069/audio"  # WebSocket URI of the Flask server
 
 logging.basicConfig(level=logging.INFO)
 
 
 async def handle_audio(websocket, path):
-    stream_id = path.replace(
-        "/socket/", "", 1
-    )  # TODO: Stream Id still broken, shows up like 'socket/c01123d6-1328-4e65-a641-6f4ec156b5c1' (check again)
+    """
+    Handle incoming audio data over WebSocket and forward it to the Flask server.
+    """
+    stream_id = path.split("/")[-1]  # Extract the stream_id from the path
     logging.info(f"Stream {stream_id} connected")
 
-    # Create a new HLSStreamManager for the stream_id if it doesn't exist
-    if stream_id not in hls_managers:
-        hls_managers[stream_id] = HLSStreamManager(
-            stream_id, HLS_OUTPUT_DIR, SERVER_HOST, HTTP_SERVER_PORT
-        )
-        hls_url = hls_managers[stream_id].get_hls_playlist_url()
-        logging.info(f"HLS URL for stream {stream_id}: {hls_url}")
-
+    # Establish a WebSocket connection to the Flask server
     try:
-        async for message in websocket:
-            if isinstance(message, bytes):
-                hls_managers[stream_id].write_audio_data(message)
+        async with websockets.connect(FLASK_SERVER_WS_URI) as flask_socket:
+            logging.info(f"Connected to Flask server at {FLASK_SERVER_WS_URI}")
+
+            # Forward the audio data to the Flask server
+            try:
+                async for message in websocket:
+                    if isinstance(message, bytes):
+                        await flask_socket.send(message)
+                        logging.info(f"Forwarded audio data for stream {stream_id}")
+            except Exception as e:
+                logging.error(
+                    f"Error while forwarding audio data for stream {stream_id}: {e}"
+                )
+
     except websockets.exceptions.ConnectionClosed:
         logging.info(f"Stream {stream_id} connection closed")
-        hls_managers[stream_id].cleanup()
-        del hls_managers[stream_id]
+    except Exception as e:
+        logging.error(f"Error while connecting to Flask server: {e}")
 
 
 async def start_server():
+    # Start the WebSocket server for handling audio data
     async with websockets.serve(handle_audio, SERVER_HOST, SERVER_PORT):
         logging.info(f"WebSocket server started on ws://{SERVER_HOST}:{SERVER_PORT}")
         await asyncio.Future()  # Run server indefinitely
